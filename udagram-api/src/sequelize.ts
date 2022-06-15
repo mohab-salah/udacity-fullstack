ba7823c2934b1c808d3ffefd1f2d502f8127a351
mohab-salah <58731357+mohab-salah@users.noreply.github.com>
2022-06-15 17:02:33 +0200

    Update sequelize.ts

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,12 +1,18 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
+export const sequelize = new Sequelize(
+  "postgres://postgres:postgres@database-1.ctza6axnjjhu.us-east-1.rds.amazonaws.com:5432/postgres"
+);
 
-  dialect: "postgres",
-  storage: ":memory:",
-});
+// export const sequelize = new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   port:config.dbport,
+
+//   dialect: "postgres",
+//   storage: ":memory:",
+// });
+
