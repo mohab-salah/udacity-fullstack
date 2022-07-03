644cb509faac25b9368bc2a3d8378bb91c2a5a8a
mohab-salah <58731357+mohab-salah@users.noreply.github.com>
2022-07-03 22:24:52 +0200

    Add files via upload

---
+++ udagram/udagram-api/src/sequelize.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+
+//  export const sequelize = new Sequelize(
+//    "postgres://postgres:postgres@database-1.cwqnyw2ecxvp.us-east-1.rds.amazonaws.com:5432/postgres"
+//  );
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.dbport,
+
+  dialect: "postgres",
+  storage: ":memory:",
+});
